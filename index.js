3cd8b8d44089bde68e7a90482743b14c4844e24a
iamtheluiz <iamtheluiz.dev@gmail.com>
2020-01-25 20:13:23 -0300

    feat: get application flags

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const clear = require('clear'); // Clear terminal
 
 // Application lib
 const showApplicationBanner = require('./lib/showApplicationBanner');
+const getApplicationFlags = require('./lib/getApplicationFlags');
 
 // Clear Terminal
 clear();
 
 showApplicationBanner();
+getApplicationFlags();
