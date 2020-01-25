65610ea87f4813cb31ede9ee5fdc2560df4d7d4d
iamtheluiz <iamtheluiz.dev@gmail.com>
2020-01-25 18:39:37 -0300

    feat: show application banner

---
+++ index.js
@@ -0,0 +1,10 @@
+#! /usr/bin/env node
+const clear = require('clear'); // Clear terminal
+
+// Application lib
+const showApplicationBanner = require('./lib/showApplicationBanner');
+
+// Clear Terminal
+clear();
+
+showApplicationBanner();
