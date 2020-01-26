1365c08cf37d365bbe24c399b10d07d99e86e963
iamtheluiz <iamtheluiz.dev@gmail.com>
2020-01-26 20:22:21 -0300

    feat: download music cover from image link

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 #! /usr/bin/env node
+const path = require('path');
 const clear = require('clear'); // Clear terminal
 
 // Application lib
@@ -6,6 +7,7 @@ const showApplicationBanner = require('./lib/showApplicationBanner');
 const getApplicationFlags = require('./lib/getApplicationFlags');
 const getMusicMetadata = require('./lib/getMusicMetadata');
 const askNewMusicMetadata = require('./lib/askNewMusicMetadata');
+const downloadMusicCoverImage = require('./lib/downloadMusicCoverImage');
 
 // Clear Terminal
 clear();
@@ -16,6 +18,9 @@ async function run() {
   if (filename = getApplicationFlags()) {
     const metadata = getMusicMetadata(filename);
     const newMetadata = await askNewMusicMetadata(metadata);
+    const coverImage = await downloadMusicCoverImage(newMetadata.cover, path.resolve(__dirname, './temp'));
+
+    console.log(coverImage);
   }
 }
 
