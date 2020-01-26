3ca6f162e42a6e4886c317537b85c9fd44e0ae75
iamtheluiz <iamtheluiz.dev@gmail.com>
2020-01-25 21:40:26 -0300

    feat: get music file metadata

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 #! /usr/bin/env node
 const clear = require('clear'); // Clear terminal
+const getAbsolutePath = require('./util/getAbsolutePath');
 
 // Application lib
 const showApplicationBanner = require('./lib/showApplicationBanner');
 const getApplicationFlags = require('./lib/getApplicationFlags');
+const getMusicMetadata = require('./lib/getMusicMetadata');
 
 // Clear Terminal
 clear();
 
 showApplicationBanner();
 if(filename = getApplicationFlags()) {
-  console.log(filename);
+  const metadata = getMusicMetadata(filename);
+
+  console.log(metadata);
 }
