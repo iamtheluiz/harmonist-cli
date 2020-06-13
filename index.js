524f690d582202c4069e7530858ebb9aa0a27b0a
iamtheluiz <iamtheluiz.dev@gmail.com>
2020-06-12 22:12:31 -0300

    feat: add download usign Youtube link

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 #! /usr/bin/env node
 const path = require('path');
+const fs = require('fs');
 const clear = require('clear'); // Clear terminal
+const chalk = require('chalk');
+const ytdl = require('ytdl-core');
+const inquirer = require('inquirer');
 
 // Application lib
 const showApplicationBanner = require('./lib/showApplicationBanner');
@@ -16,11 +20,30 @@ clear();
 showApplicationBanner();
 
 async function run() {
-  if (filename = getApplicationFlags()) {
-    const metadata = getMusicMetadata(filename);
-    let newMetadata = await askNewMusicMetadata(metadata);
-    newMetadata.APIC = await downloadMusicCoverImage(newMetadata.cover, path.resolve(__dirname, './temp'));
-    setMusicMetadata(newMetadata, filename);
+  if (context = getApplicationFlags()) {
+    let { type, content } = context;
+
+    if (type === 'link') {
+      try {
+        console.log(chalk.yellow('Downloading file...'));
+        const { filename } = await inquirer.prompt({ type: 'input', name: 'filename', message: 'Filename:' });
+
+        const stream = await ytdl(content, { filter: format => format.container === 'mp3' });
+
+        stream.pipe(fs.createWriteStream(`${filename}.mp3`));
+
+        stream.on('end', () => {
+          console.log(chalk.greenBright('Done!'));
+        });
+      } catch (error) {
+        console.log(chalk.red("This music doesn't support '.mp3' format!"));
+      }
+    } else {
+      const metadata = getMusicMetadata(content);
+      let newMetadata = await askNewMusicMetadata(metadata);
+      newMetadata.APIC = await downloadMusicCoverImage(newMetadata.cover, path.resolve(__dirname, './temp'));
+      setMusicMetadata(newMetadata, content);
+    }
   }
 }
 
